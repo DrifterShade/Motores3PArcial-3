3dd155968e19cb31f43b28486736bb8bb25995aa
DrifterShade <katyzelda@gmail.com>
2025-01-27 19:55:38 -0600

    colliders items

--- Assets/Ilse/PlayerChangePosition.cs
+++ Assets/Ilse/PlayerChangePosition.cs
@@ -22,6 +22,15 @@ public class PlayerChangePosition : MonoBehaviour
          this.transform.position = new Vector3(11.98f,2.94f,0);
 
     }
+
+  if (collision.gameObject.tag == "derecha1")
+    {
+        cam.transform.position = new Vector3(19.19f,-0.04f,-10);
+         this.transform.position = new Vector3(27.78f,1.2f,0);
+
+    }
+
+
      if (collision.gameObject.tag == "downm1")
     {
         cam.transform.position = new Vector3(0,-0.53f,-10);
@@ -38,6 +47,11 @@ public class PlayerChangePosition : MonoBehaviour
         this.transform.position = new Vector3(1.26f,3.69f,0);
     }
     
+       if (collision.gameObject.tag == "leftm1")
+    {
+        cam.transform.position = new Vector3(0,-0.53f,-10);
+        this.transform.position = new Vector3(-8.06f,1.26f,0);
+    }
 
 
 
@@ -51,14 +65,30 @@ public class PlayerChangePosition : MonoBehaviour
 
     }
 
+    
+    if (collision.gameObject.tag == "cleft")
+    {
+        cam.transform.position = new Vector3(-19.93f,-0.09f,-10f);
+         this.transform.position = new Vector3(-12.41f,1.92f,0);
+
+    }
+
   
 
-if (collision.gameObject.tag == "cup")
+    if (collision.gameObject.tag == "cup")
     {
          cam.transform.position = new Vector3(0.02f,11.67f,-10);
          this.transform.position = new Vector3(1.5f,7.76f,0);
 
     }
+    if (collision.gameObject.tag == "derder")
+    {
+         cam.transform.position = new Vector3(40.31f,-0.09f,-10f);
+         this.transform.position = new Vector3(32.07f,2.83f,0);
+
     }
-   }
+    }
+    }
+    
+   
 
